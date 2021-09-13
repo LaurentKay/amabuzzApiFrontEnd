d6d01f3f1a777fb98755e5565be2d54ce89f4d3c
Laurent <laurentkayembe@gmail.com>
2021-09-13 10:22:45 +0200

    Back to http

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const options = {
   cert: fs.readFileSync('cert.pem'),
   app
 }
-const server = require('https').createServer(options);
+const server = require('http').createServer(app);
 
 // app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({limit: '50mb'}));
