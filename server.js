3b5498255da4e069deac76a5e744792746382da7
Laurent <laurentkayembe@gmail.com>
2022-01-24 17:11:25 +0200

    File upload size limit increased

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const fileUpload = require('express-fileupload');
 const server = require('http').createServer(app);
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 //app.use(bodyParser.json({limit: '50mb'}));
 //app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 //app.use(bodyParser.urlencoded({ extended: false }));
