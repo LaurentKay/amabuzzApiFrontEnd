6ec5dba26c54b45449a7db8ce96d9a72990b17c2
Laurent <laurentkayembe@gmail.com>
2022-01-25 14:34:23 +0200

    File updalod testing

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 // app.use(express.json({ extended: false, limit: '50mb'}));
 // app.use(express.urlencoded({limit: '50mb', extended: false, parameterLimit:50000}));
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:500000}));
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
 app.use(cookieParser());
